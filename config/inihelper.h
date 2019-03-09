9d11371f5545c14d1c46ef1be72e056c17ed04b5
Peefy <xpf6677@163.com>
2019-03-09 15:29:37 +0800

    update json files

--- config/inihelper.h
+++ config/inihelper.h
@@ -9,6 +9,15 @@
 #include "../TYPE_DEF.H"
 #include "../communication/sixdof.h"
 
+#define JSON_PLATFORM_FILE_NAME           "platformconfig.json"
+
+#define JSON_PlaneAboveHingeLength_KEY        "upToUpHingeLength"    //上平面到上铰支座距离
+#define JSON_PlaneAboveBottomLength_KEY       "upToDownHingeLength"  //上平面到下铰支座距离
+#define JSON_CircleTopRadius_KEY              "upCircleD"            //小圆直径
+#define JSON_CircleBottomRadius_KEY           "downCircleD"          //大圆直径
+#define JSON_DistanceBetweenHingeTop_KEY      "upHingesDis"          //两两上铰支座中心距离
+#define JSON_DistanceBetweenHingeBottom_KEY   "downHingesDis"        //两两下铰支座中心距离
+
 namespace config {
     void GenerateDefaultConfigFile();
     void ReadAll(bool& result, int& baud, int& portnum);
